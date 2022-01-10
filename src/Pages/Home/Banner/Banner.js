b8ffe7122b1c086a3dfb9e73c06e01d72160526d
tashdit <tajdiganta@gmail.com>
2022-01-10 13:09:18 +0600

    second push

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
             <section className="d-flex align-items-center hero">
                 <div className="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
                     <h1>Your New Online Presence with MMH Books</h1>
-                    <h2>We are team of talented designers making websites with Mollah Masuk Helal</h2>
+                    <h2>We are team of talented selling books with Mollah Masuk Helal</h2>
                     <a href="#about" className="btn-get-started scrollto">Get Started</a>
                 </div>
             </section>
